920109bba7a8a3bc3c7375bcc0042d399336ec3d
Yao Cui <yc2554@Yaos-MBP.home>
2018-02-18 23:17:34 -0500

    assignment1-task1

---
+++ assignment1/task0/map.py
@@ -0,0 +1,21 @@
+import sys
+#import os
+
+for line in sys.stdin:
+    #print('0\t1 2 3 4 5')
+    continue
+    # currfile = os.environ.get('mapreduce_map_input_file')
+    # currfile = '1'
+    # line = line.strip()
+    # entry = line.split(",")
+
+    # if 'open' in currfile:
+    #     summon_number = entry[0]
+    #     print('{0:s}\t{1:s}'.format(summon_number, '0'))
+    # else:
+    #     summon_number = entry[0]
+    #     plate_id = entry[14]
+    #     violation_precinct = entry[6]
+    #     violation_code = entry[2]
+    #     issue_date = entry[1]
+    #     print('{0:s}\t{1:s} {2:s} {3:s} {4:s} {5:s}'.format(summon_number, '1', plate_id, violation_precinct, violation_code, issue_date))
--- assignment1/task1/reduce.py
+++ assignment1/task1/reduce.py
@@ -1,32 +1,73 @@
+#!/usr/bin/env python
+#Reduce function for computing matrix multiply A*B
+
+#Input arguments:
+#variable n should be set to the inner dimension of the matrix product (i.e., the number of columns of A/rows of B)
+
 import sys
+import string
+#import numpy
 
-openkey = None
-parkingdic = {}
-currentkey = None
+# #number of columns of A/rows of B
+# n = int(sys.argv[1]) 
 
+# #Create data structures to hold the current row/column values (if needed; your code goes here)
+
+# dic = {}
+# total = 0
+# currentkey = None
+
+# input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
-    continue
-#     line = line.strip()
-#     key, value = line.split('\t', 1)
-#     entry = value.split(' ')
+	continue
+# 	#Remove leading and trailing whitespace
+# 	line = line.strip()
 
-#     if key == currentkey:
-#         if entry[0] == '0': #from open
-#             openkey = key
-#         else: #from parking
-#             parkingdic[key] = entry
-#     else:
-#         for k, v in parkingdic.items():
-#             if k != openkey:
-#                 print('{0:s}\t{1:s}, {2:s}, {3:s}, {4:s}'.format(k, v[1], v[2], v[3], v[4]))
-#         currentkey = key
-#         openkey = None
-#         parkingdic = {}
-#         if entry[0] == '0': #from open
-#             openkey = key
-#         else: #from parking
-#             parkingdic[key] = entry
+# 	#Get key/value 
+# 	key, value = line.split('\t',1)
 
-# for k, v in parkingdic.items():
-#     if k != openkey:
-#         print('{0:s}\t{1:s}, {2:s}, {3:s}, {4:s}'.format(k, v[1], v[2], v[3], v[4]))
+# 	#Parse key/value input (your code goes here)
+
+# 	entry = value.split(' ')
+# 	MatrixName = entry[0]
+# 	j = entry[1]
+# 	subKey = (MatrixName, j)
+# 	try:
+# 		subValue = float(entry[2])
+# 	except ValueError:
+# 		continue
+
+# 	#If we are still on the same key...
+# 	if key==currentkey:
+
+# 		#Process key/value pair (your code goes here)
+# 		searchMatrix = 'A'
+# 		if MatrixName == 'A':
+# 			searchMatrix = 'B'
+
+# 		searchKey = (searchMatrix, j)
+# 		if searchKey in dic:
+# 			total += subValue * dic[searchKey]
+# 		else:
+# 		#Otherwise, if this is a new key...
+# 			dic[subKey] = subValue
+
+
+# 	else:
+# 		#If this is a new key and not the first key we've seen
+# 		if currentkey:
+
+# 			#compute/output result to STDOUT (your code goes here)
+# 			print('{0:s}\t{1:f}'.format(currentkey, total))
+
+# 		currentkey = key
+
+# 		#Process input for new key (your code goes here)
+# 		total = 0
+# 		dic = {subKey: subValue}
+
+
+
+# #Compute/output result for the last key (your code goes here)
+# if currentkey == key:
+# 	print('{0:s}\t{1:f}'.format(currentkey, total))
