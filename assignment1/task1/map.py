e01a161ff80f1efc95e453c636aa7b24f03ba535
Yao Cui <yc2554@Yaos-MBP.home>
2018-02-18 23:26:40 -0500

    assignment1-task1

--- assignment1/task1/map.py
+++ assignment1/task1/map.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # map function for matrix multiply
-#Input file assumed to have lines of the form "A,i,j,x", where i is the row index, j is the column index, and x is the value in row i, column j of A. Entries of A are followed by lines of the form "B,i,j,x" for the matrix B. 
-#It is assumed that the matrix dimensions are such that the product A*B exists. 
+# Input file assumed to have lines of the form "A,i,j,x", where i is the row index, j is the column index, and x is the value in row i, column j of A. Entries of A are followed by lines of the form "B,i,j,x" for the matrix B.
+# It is assumed that the matrix dimensions are such that the product A*B exists.
 
-#Input arguments:
-#m should be set to the number of rows in A, p should be set to the number of columns in B.
- 
+# Input arguments:
+# m should be set to the number of rows in A, p should be set to the number of columns in B.
+
 import sys
-import string
-#import numpy
+import os
+# import numpy
 
 # #number of rows in A
-# m = int(sys.argv[1]) 
+# m = int(sys.argv[1])
 
 # #number of columns in B
 # p = int(sys.argv[2])
@@ -19,32 +19,20 @@ import string
 
 # input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
-    continue
-	# #Remove leading and trailing whitespace
-	# line = line.strip()
+	currfile = os.environ.get('mapreduce_map_input_file')
+	line = line.strip()
+	entry = line.split(",")
 
-	# #Split line into array of entry data
-	# entry = line.split(",")
-    
-	# # Set row, column, and value for this entry
-	# row = int(entry[1])
-	# col = int(entry[2])
-	# value = float(entry[3])
-
-	# #If this is an entry in matrix A...
-	# if (entry[0] == "A"):
-		
-	# 	#Generate the necessary key-value pairs
- 	# 	#(your code goes here)
-	# 	for k in range(p):
-	# 		print('{0:d} {1:d}\t{2:s} {3:d} {4:f}'.format(row, k, 'A', col, value))
-	# #Otherwise, if this is an entry in matrix B...
-	# else:
-		
-	# 	#Generate the necessary key-value pairs
- 	# 	#(your code goes here)
-	# 	for i in range(m):
-	# 		print('{0:d} {1:d}\t{2:s} {3:d} {4:f}'.format(i, col, 'B', row, value))
+	if 'open' in currfile:
+		summon_number = entry[0]
+		print('{0:s}\t{1:s}'.format(summon_number, '0'))
+	else:
+		summon_number = entry[0]
+		plate_id = entry[14]
+		violation_precinct = entry[6]
+		violation_code = entry[2]
+		issue_date = entry[1]
+		print('{0:s}\t{1:s} {2:s} {3:s} {4:s} {5:s}'.format(summon_number, '1', plate_id, violation_precinct, violation_code, issue_date)) 
 
 	
         
