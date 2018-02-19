cb76c38d36642583e786144da48a7cb786e6d3ef
Yao Cui <yc2554@Yaos-MBP.home>
2018-02-18 21:55:45 -0500

    assignment1-task1

--- assignment1/task1/reduce.py
+++ assignment1/task1/reduce.py
@@ -5,28 +5,28 @@ parkingdic = {}
 currentkey = None
 
 for line in sys.stdin:
+    continue
+#     line = line.strip()
+#     key, value = line.split('\t', 1)
+#     entry = value.split(' ')
 
-    line = line.strip()
-    key, value = line.split('\t', 1)
-    entry = value.split(' ')
+#     if key == currentkey:
+#         if entry[0] == '0': #from open
+#             openkey = key
+#         else: #from parking
+#             parkingdic[key] = entry
+#     else:
+#         for k, v in parkingdic.items():
+#             if k != openkey:
+#                 print('{0:s}\t{1:s}, {2:s}, {3:s}, {4:s}'.format(k, v[1], v[2], v[3], v[4]))
+#         currentkey = key
+#         openkey = None
+#         parkingdic = {}
+#         if entry[0] == '0': #from open
+#             openkey = key
+#         else: #from parking
+#             parkingdic[key] = entry
 
-    if key == currentkey:
-        if entry[0] == '0': #from open
-            openkey = key
-        else: #from parking
-            parkingdic[key] = entry
-    else:
-        for k, v in parkingdic.items():
-            if k != openkey:
-                print('{0:s}\t{1:s}, {2:s}, {3:s}, {4:s}'.format(k, v[1], v[2], v[3], v[4]))
-        currentkey = key
-        openkey = None
-        parkingdic = {}
-        if entry[0] == '0': #from open
-            openkey = key
-        else: #from parking
-            parkingdic[key] = entry
-
-for k, v in parkingdic.items():
-    if k != openkey:
-        print('{0:s}\t{1:s}, {2:s}, {3:s}, {4:s}'.format(k, v[1], v[2], v[3], v[4]))
+# for k, v in parkingdic.items():
+#     if k != openkey:
+#         print('{0:s}\t{1:s}, {2:s}, {3:s}, {4:s}'.format(k, v[1], v[2], v[3], v[4]))
