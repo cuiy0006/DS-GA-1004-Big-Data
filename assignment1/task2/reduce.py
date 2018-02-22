fd53b133047c83548fe26acd0076d060b3bf5183
Yao Cui <yc2554@172-16-34-49.DYNAPOOL.NYU.EDU>
2018-02-22 15:37:31 -0500

    task2

--- assignment1/task2/reduce.py
+++ assignment1/task2/reduce.py
@@ -6,17 +6,16 @@ cnt = 0
 currentkey = None
 
 for line in sys.stdin:
-	continue
-# 	line = line.strip()
-# 	key, value = line.split('\t', 1)
+	line = line.strip()
+	key, value = line.split('\t', 1)
 	
-# 	if key == currentkey:
-# 		cnt += 1
-# 	else:
-# 		if currentkey:
-# 			print('{0:s}\t{1:d}'.format(currentkey, cnt))
-# 		currentkey = key
-# 		cnt = 1
+	if key == currentkey:
+		cnt += 1
+	else:
+		if currentkey:
+			print('{0:s}\t{1:d}'.format(currentkey, cnt))
+		currentkey = key
+		cnt = 1
 
-# if currentkey == key:
-# 	print('{0:s}\t{1:d}'.format(currentkey, cnt))
+if currentkey != None:
+	print('{0:s}\t{1:d}'.format(currentkey, cnt))
