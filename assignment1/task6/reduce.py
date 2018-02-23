e6c65b82b437a96f0d534d756677d7367bf0a6b0
Yao Cui <yc2554@Yaos-MBP.home>
2018-02-23 15:10:21 -0500

    task6

--- assignment1/task6/reduce.py
+++ assignment1/task6/reduce.py
@@ -15,11 +15,47 @@ for line in sys.stdin:
 	if key == currentkey:
 		cnt += 1
 	else:
-		if max_cnt < cnt:
-			max_key = currentkey
-			max_cnt = cnt
+		if currentkey != None:
+			if len(maxheap) < 10:
+				heappush(maxheap, (cnt, currentkey))
+			else:
+				if cnt > maxheap[0][0]:
+					heappop(maxheap)
+					heappush(maxheap, (cnt, currentkey))
+
+			if len(minheap) < 10:
+				heappush(minheap, (-cnt, currentkey))
+			else:
+				if -cnt > minheap[0][0]:
+					heappop(minheap)
+					heappush(maxheap, (-cnt, currentkey))
+
 		currentkey = key
 		cnt = 1
 
-if max_key != None:
-	print('{0:s}\t{1:d}'.format(max_key, max_cnt))
+if currentkey != None:
+	if len(maxheap) < 10:
+		heappush(maxheap, (cnt, currentkey))
+	else:
+		if cnt > maxheap[0][0]:
+			heappop(maxheap)
+			heappush(maxheap, (cnt, currentkey))
+
+	if len(minheap) < 10:
+		heappush(minheap, (-cnt, currentkey))
+	else:
+		if -cnt > minheap[0][0]:
+			heappop(minheap)
+			heappush(maxheap, (-cnt, currentkey))
+
+res = []
+while len(maxheap) != 0:
+	cnt, key = heappop(maxheap)
+	res.append((key, cnt))
+res.reverse()
+while len(minheap) != 0:
+	cnt, key = heappop(minheap)
+	res.append((key, -cnt))
+for key, cnt in res:
+	print('{0:s}\t{1:d}'.format(key, cnt)) 
+
