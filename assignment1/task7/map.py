59573946e43736a9886a7cbcb4506599ffef493d
Yao Cui <yc2554@Yaos-MBP.home>
2018-02-23 23:10:32 -0500

    task7 revise

--- assignment1/task7/map.py
+++ assignment1/task7/map.py
@@ -2,16 +2,14 @@
 
 import sys
 import os
+import string
 
 # input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
 	line = line.strip()
 	entry = line.split(",")
 
-	violation_code = entry[2]
-	issue_date = entry[1]
+	violation_code = entry[2].strip(string.punctuation).strip()
+	issue_date = entry[1].strip(string.punctuation).strip()
 
 	print('{0:s}\t{1:s}'.format(violation_code, issue_date))
-
-	
-        
