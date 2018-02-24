8dc93333bd00f8dce90882fce46ade9ebc0e1028
Yao Cui <yc2554@Yaos-MBP.home>
2018-02-23 22:26:46 -0500

    task5 revise

--- assignment1/task5/map.py
+++ assignment1/task5/map.py
@@ -2,14 +2,15 @@
 
 import sys
 import os
+import string
 
 # input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
 	line = line.strip()
 	entry = line.split(",")
 
-	plate_id = entry[14]
-	registration_state = entry[16]
+	plate_id = entry[14].strip(string.punctuation).strip()
+	registration_state = entry[16].strip(string.punctuation).strip()
 
 	print('{0:s}, {1:s}\t{2:s}'.format(plate_id, registration_state, '1')) 
 
