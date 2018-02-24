f7036de883746cfc01db710ace56129a717b86f8
Yao Cui <yc2554@Yaos-MBP.home>
2018-02-23 22:13:37 -0500

    task4 revise

--- assignment1/task4/map.py
+++ assignment1/task4/map.py
@@ -2,13 +2,14 @@
 
 import sys
 import os
+import string
 
 # input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
 	line = line.strip()
 	entry = line.split(",")
 
-	registration_state = entry[16]
+	registration_state = entry[16].strip(string.punctuation).strip()
 	if registration_state != 'NY':
 		registration_state = 'Other'
 		
