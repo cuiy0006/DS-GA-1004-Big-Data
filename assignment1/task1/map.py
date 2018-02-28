decb730d0fab38ee85593a30f82a15614c30d23c
Yao Cui <yc2554@Yaos-MBP.home>
2018-02-28 15:33:53 -0500

    change

--- assignment1/task1/map.py
+++ assignment1/task1/map.py
@@ -22,8 +22,6 @@ for line in sys.stdin:
 	else:
 		summon_number = entry[0]
 		plate_id = entry[-8]
-		if plate_id == 'T':
-			continue
 		violation_precinct = entry[-16]
 		violation_code = entry[2]
 		issue_date = entry[1]
