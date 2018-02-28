630b4d8530baac6eacc807dae9e10ee7f2fa7c64
Yao Cui <yc2554@Yaos-MBP.home>
2018-02-28 16:38:38 -0500

    change

--- assignment1/task8/map.py
+++ assignment1/task8/map.py
@@ -8,13 +8,21 @@ import re
 # input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
 	line = line.strip()
-	line = re.sub(r'\".*?\"','2',line)
-	entry = line.split(",")
+	matches = re.findall(r'\".*?\"', line)
+	lst = list(map(lambda s:s.replace(',',''), matches))
+	for i, match in enumerate(matches):
+		line = line.replace(match, lst[i])
+	entry = line.split(',')
 
 	vehicle_make = entry[-2]
 	vehicle_color = entry[-3]
-	if vehicle_make != '':
-		print('{0:s},{1:s}\t{2:s}'.format('vehicle_make', vehicle_make, '1'))
-	if vehicle_color != '':
-		print('{0:s},{1:s}\t{2:s}'.format('vehicle_color', vehicle_color, '1'))
+
+	if vehicle_make == '':
+		vehicle_make = 'NONE'
+
+	print('{0:s},{1:s}\t{2:s}'.format('vehicle_make', vehicle_make, '1'))
+	if vehicle_color == '':
+		vehicle_color = 'NONE'
+
+	print('{0:s},{1:s}\t{2:s}'.format('vehicle_color', vehicle_color, '1'))
         
