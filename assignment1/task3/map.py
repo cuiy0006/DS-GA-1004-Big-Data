b76fe5e927e1ace4abc81f3f58d90674b8d793c2
Yao Cui <yc2554@Yaos-MBP.home>
2018-02-28 15:42:07 -0500

    change

--- assignment1/task3/map.py
+++ assignment1/task3/map.py
@@ -3,19 +3,16 @@
 import sys
 import os
 import string
+import re
 
 # input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
 	line = line.strip()
 	entry = line.split(",")
 
-	license_type = entry[2].strip().strip(string.punctuation).strip()
-	amount_due = entry[12].strip().strip(string.punctuation).strip()
-	if license_type == 'NULL' or license_type == 'JZ' or license_type =='L744':
-		continue
-
-	if license_type=='1630':
-		license_type = entry[2]
+	line = re.sub(r'\".*?\"','1',line)
+	license_type = entry[2]
+	amount_due = entry[12]
 
 	print('{0:s}\t{1:s}'.format(license_type, amount_due))
 
