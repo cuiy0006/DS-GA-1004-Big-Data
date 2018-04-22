84fa457acdd7ff1820db029773ce4135ef7528fb
cuiy0006 <yc2554@nyu.edu>
2018-04-22 14:25:37 -0400

    lab8 end

--- lab8/freqitems.py
+++ lab8/freqitems.py
@@ -11,8 +11,10 @@ if __name__ == "__main__":
     sc = SparkContext(appName="FreqItems")
     data = sc.textFile(sys.argv[1])
     transactions = data.map(lambda line: line.strip().split(','))
-    model = FPGrowth.train(transactions, minSupport=0.1, numPartitions=10)
+    model = FPGrowth.train(transactions, minSupport=0.02, numPartitions=10)
     results = (model.freqItemsets()).collect()
+    results = list(filter(lambda x:len(x.items)>=2, results))
+    results.sort(key=lambda x:-x.freq)
     for line in results:
         print("%s" % (str(line)))
 
